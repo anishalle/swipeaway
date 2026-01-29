ea89bed1a9e321dee1f624292e95cb741968fa2f
anishalle <anish.alle11@gmail.com>
2026-01-28 19:44:14 -0600

    Initial Commit

---
+++ swipeaway/swipeawayApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeawayApp.swift
+//  swipeaway
+//
+//  Created by Anish Alle on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct swipeawayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swipeawayTests/swipeawayTests.swift
@@ -0,0 +1,17 @@
+//
+//  swipeawayTests.swift
+//  swipeawayTests
+//
+//  Created by Anish Alle on 1/28/26.
+//
+
+import Testing
+@testable import swipeaway
+
+struct swipeawayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
